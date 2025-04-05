e17d33eccea00f3dca405d5013039ea79194ff61
AdriCU21 <adriancamachoutrera@a.cifpperedesongall.org>
2025-04-05 11:41:49 +0200

    He terminado la funcion 1 del programa, quedaria instanciar Mockito y Junit para hacer los test

--- src/AirLlucmajor/añadirVuelosDisp.java
+++ src/AirLlucmajor/añadirVuelosDisp.java
@@ -1,4 +1,29 @@
 package AirLlucmajor;
 
 public class añadirVuelosDisp {
+
+     int dia;
+     int mes;
+     int año;
+     double precio;
+     String origen;
+     String destino;
+     boolean estado;
+
+
+
+    public añadirVuelosDisp(int dia, int mes, int año, double precio, String origen, String destino, boolean estado) {
+        this.dia = dia;
+        this.mes = mes;
+        this.año = año;
+        this.precio = precio;
+        this.origen = origen;
+        this.destino = destino;
+        this.estado = estado;
+    }
+
+    public añadirVuelosDisp() {
+    }
+
+
 }
--- src/main.java
+++ src/main.java
@@ -1,8 +1,13 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
+import AirLlucmajor.añadirVuelosDisp;
 public class main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        ArrayList<añadirVuelosDisp> vuelos = new ArrayList<>();
+
         System.out.println("✈️  ===== MENÚ DE GESTIÓN DE VUELOS DE AIRLLUCMAJOR ===== ✈️");
         System.out.println("1️⃣  ✍️ Añadir vuelos disponibles");
         System.out.println("2️⃣  📅 Consultar los vuelos disponibles por día y por semana");
@@ -13,46 +18,72 @@ public class main {
         System.out.println("7️⃣  🚪 Salir");
         System.out.print("🛠️ Seleccione una opción: ");
 
+        añadirVuelosDisp añadirVuelosD = new añadirVuelosDisp();
         int opcion = scanner.nextInt();
 
         while (opcion != 7) {
 
-                if (opcion == 1) {
-                    System.out.println("🛫 Añadiendo vuelos disponibles...");
-                    //Funcion 1
+            if (opcion == 1) {
+                System.out.println("🛫 Añadiendo vuelos disponibles...");
+                //Funcion 1
+                System.out.print("Día del vuelo: ");
+                int dia = scanner.nextInt();
 
-                    // Lógica para añadir vuelos Adri
-                } else if (opcion == 2) {
-                    System.out.println("📆 Consultando vuelos disponibles...");
-                    //Funcion 2
+                System.out.print("Mes del vuelo: ");
+                int mes = scanner.nextInt();
 
-                    // Lógica para consultar vuelos Miguel Angel
-                } else if (opcion == 3) {
-                    System.out.println("🛎️ Reservando un vuelo...");
-                    //Funcion 3
+                System.out.print("Año del vuelo: ");
+                int año = scanner.nextInt();
 
-                    // Lógica para reservar un vuelo Gemelo
-                } else if (opcion == 4) {
-                    System.out.println("🧐 Consultando vuelos reservados...");
-                    //Funcion 4
+                System.out.print("Precio del vuelo: ");
+                double precio = scanner.nextDouble();
+                scanner.nextLine();
 
-                    // Lógica para consultar vuelos reservados Adri
-                } else if (opcion == 5) {
-                    System.out.println("🚫 Cancelando una reserva...");
-                    //Funcion 5
+                System.out.print("Origen del vuelo: ");
+                String origen = scanner.nextLine();
 
-                    // Lógica para cancelar una reserva Miguel Angel
-                } else if (opcion == 6) {
-                    System.out.println("📨 Enviando notificaciones...");
-                    //Funcion 6
+                System.out.print("Destino del vuelo: ");
+                String destino = scanner.nextLine();
 
-                    // Lógica para enviar notificaciones Gemelo
-                } else if (opcion == 7) {
-                    System.out.println("👋 Saliendo del sistema... ¡Hasta pronto!");
-                    break;
-                } else {
-                    System.out.println("❌ Entrada no válida. Introduzca un número del 1 al 7.");
-                }
+                añadirVuelosDisp nuevoVuelo = new añadirVuelosDisp(dia, mes, año, precio, origen, destino, true);
+                
+                vuelos.add(nuevoVuelo);
+
+
+
+                System.out.println("✔️ Vuelo añadido correctamente!");
+
+            } else if (opcion == 2) {
+                System.out.println("📆 Consultando vuelos disponibles...");
+                //Funcion 2
+
+                // Lógica para consultar vuelos Miguel Angel
+            } else if (opcion == 3) {
+                System.out.println("🛎️ Reservando un vuelo...");
+                //Funcion 3
+
+                // Lógica para reservar un vuelo Gemelo
+            } else if (opcion == 4) {
+                System.out.println("🧐 Consultando vuelos reservados...");
+                //Funcion 4
+
+                // Lógica para consultar vuelos reservados Adri
+            } else if (opcion == 5) {
+                System.out.println("🚫 Cancelando una reserva...");
+                //Funcion 5
+
+                // Lógica para cancelar una reserva Miguel Angel
+            } else if (opcion == 6) {
+                System.out.println("📨 Enviando notificaciones...");
+                //Funcion 6
+
+                // Lógica para enviar notificaciones Gemelo
+            } else if (opcion == 7) {
+                System.out.println("👋 Saliendo del sistema... ¡Hasta pronto!");
+                break;
+            } else {
+                System.out.println("❌ Entrada no válida. Introduzca un número del 1 al 7.");
+            }
 
             System.out.println("✈️  ===== MENÚ DE GESTIÓN DE VUELOS DE AIRLLUCMAJOR ===== ✈️");
             System.out.println("1️⃣  ✍️ Añadir vuelos disponibles");
